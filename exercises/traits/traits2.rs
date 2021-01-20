1ac7be716a6bd36a447644e50aaad9d745922690
Syrone Wong <wong.syrone@gmail.com>
2021-01-20 11:56:20 +0800

    more
    Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,13 +10,18 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 //TODO: Add your code here
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut mself = self;
+        mself.push(String::from("Bar"));
+        mself
+    }
+}
 
 #[cfg(test)]
 mod tests {
